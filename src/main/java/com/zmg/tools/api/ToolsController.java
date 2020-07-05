1f942de045d6cd7499582ac57351af39d10e60f2
曾敏桂 <mingui.zeng@zhenai.com>
2020-07-05 19:25:46 +0800

    增加简易版消息队列功能

--- src/main/java/com/zmg/tools/api/ToolsController.java
+++ src/main/java/com/zmg/tools/api/ToolsController.java
@@ -1,9 +1,11 @@
 package com.zmg.tools.api;
 
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.zmg.tools.client.UDPClient;
 import com.zmg.tools.util.CommUtils;
-import com.zmg.tools.util.SpringContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @RestController
 @Controller
@@ -20,6 +24,11 @@ public class ToolsController {
 
     @Autowired
     private UDPClient udpClient;
+
+    @Value("${token}")
+    private String _token;
+
+    private Map<String,Map<String, ConcurrentLinkedQueue<JSONObject>>> messageQueueMap = new ConcurrentHashMap<>();
 
     //wake on lan
     @ResponseBody
@@ -48,6 +57,69 @@ public class ToolsController {
         result.put("message","success");
         result.put("host",request.getRemoteHost());
         result.put("port",request.getRemotePort());
+        return result;
+    }
+
+    //push message
+    @ResponseBody
+    @RequestMapping(path = {"/api/pushMessage"},method = {RequestMethod.POST})
+    public Map<String,Object> pushMessage(HttpServletRequest request) throws Exception{
+        Map<String,Object> result = new HashMap<>();
+        String appId = request.getParameter("appId");
+        String queueName = request.getParameter("queueName");
+        String token = request.getParameter("token");
+        JSONObject messageObj = JSONObject.parseObject(request.getParameter("message"));
+
+
+        Map<String,ConcurrentLinkedQueue<JSONObject>> queueMap = messageQueueMap.containsKey(appId)?messageQueueMap.get(appId):new ConcurrentHashMap<>();
+        messageQueueMap.put(appId,queueMap);
+        ConcurrentLinkedQueue<JSONObject> queue = queueMap.containsKey(queueName)?queueMap.get(queueName):new ConcurrentLinkedQueue<>();
+        queueMap.put(queueName,queue);
+
+        if(!token.equalsIgnoreCase(_token)){
+           result.put("code",1);
+           result.put("message","invalid token");
+           return result;
+        }
+
+        queue.add(messageObj);
+        result.put("code",0);
+        result.put("message","success");
+        return result;
+    }
+
+    //pull message
+    @RequestMapping(path = {"/api/pullMessage"},method = {RequestMethod.GET,RequestMethod.POST})
+    public Map<String,Object> pullMessage(HttpServletRequest request) throws Exception{
+        Map<String,Object> result = new HashMap<>();
+        String appId = request.getParameter("appId");
+        String queueName = request.getParameter("queueName");
+        String token = request.getParameter("token");
+
+        if(!messageQueueMap.containsKey(appId)){
+            messageQueueMap.put(appId,new ConcurrentHashMap<>());
+        }
+
+        if(!messageQueueMap.get(appId).containsKey(queueName)){
+            messageQueueMap.get(appId).put(queueName,new ConcurrentLinkedQueue<>());
+        }
+
+        if(!token.equalsIgnoreCase(_token)){
+            result.put("code",1);
+            result.put("message","invalid token");
+            return result;
+        }
+        ConcurrentLinkedQueue<JSONObject> queue = messageQueueMap.get(appId).get(queueName);
+
+        if(!queue.iterator().hasNext()){
+            result.put("code",1);
+            result.put("message","queue is empty");
+            return result;
+        }
+
+        result.put("code",0);
+        result.put("message",queue.iterator().next().toJSONString());
+        queue.remove();
         return result;
     }
 }
